b0650a2541571237f3d649ac7f730ec0974ea3ab
a1unade <tags0956@gmail.com>
2026-05-12 21:32:29 +0300

    [add] build ci pipeline

--- eslint.config.js
+++ eslint.config.js
@@ -2,14 +2,26 @@ import tseslint from 'typescript-eslint';
 import prettierPlugin from 'eslint-plugin-prettier';
 
 export default tseslint.config(
-  ...tseslint.configs.recommended,
   {
-    files: ['src/**/*.{ts,tsx,js,jsx}'],
+    ignores: [
+      '**/dist/**',
+      '**/dist/**/*',
+      '**/node_modules/**',
+      '**/coverage/**',
+      '**/.turbo/**',
+      '**/*.d.ts',
+    ],
+  },
+
+  ...tseslint.configs.recommended,
+
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
     plugins: {
       prettier: prettierPlugin,
     },
     rules: {
       'prettier/prettier': ['error', { singleQuote: true, semi: true }],
     },
-  }
+  },
 );
