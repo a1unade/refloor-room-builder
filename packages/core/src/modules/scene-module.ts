358cf3487e8a485df8619b30b1c087b2cb7c3ddf
a1unade <tags0956@gmail.com>
2026-05-18 00:41:58 +0300

    remove grid helper

--- packages/core/src/modules/scene-module.ts
+++ packages/core/src/modules/scene-module.ts
@@ -18,20 +18,12 @@ export class SceneModule implements IRuntimeModule, IMeshApi {
   /** Объекты, добавленные в сцену через модуль */
   private _objects: THREE.Object3D[] = [];
 
-  /** Сетка сцены */
-  private _grid: THREE.GridHelper | null = null;
-
   /** Базовый свет сцены */
   private _light: THREE.HemisphereLight | null = null;
 
   public constructor(@inject('ISceneApi') private _api: ISceneApi) {}
 
   public init(): void {
-    // Сетка
-    this._grid = new THREE.GridHelper(10, 10);
-    this._grid.position.y = -0.001;
-    this._api.addToScene(this._grid);
-
     // Свет
     this._light = new THREE.HemisphereLight(0xffffff, 0x444444, 0.6);
     this._api.addToScene(this._light);
@@ -72,21 +64,13 @@ export class SceneModule implements IRuntimeModule, IMeshApi {
   }
 
   /** Освобождает ресурсы модуля */
-  public dispose(): Promise<void> | void {
+  public dispose(): void {
     // Объекты сцены
     for (const object of this._objects) {
       this._api.removeFromScene(object);
     }
 
     this._objects.length = 0;
-
-    // Сетка
-    if (this._grid) {
-      this._api.removeFromScene(this._grid);
-      this._grid.geometry.dispose();
-      (this._grid.material as THREE.Material).dispose();
-      this._grid = null;
-    }
 
     // Свет
     if (this._light) {
